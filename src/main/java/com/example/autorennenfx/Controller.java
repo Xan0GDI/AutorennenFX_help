0c06b69e0248ee8e71ab88e4b459fc91a2b59ae5
Xan0GDI <michaelh1109@outlook.de>
2025-11-10 10:30:15 +0100

    yes

---
+++ src/main/java/com/example/autorennenfx/Controller.java
@@ -0,0 +1,16 @@
+package com.example.autorennenfx;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+public class Controller {
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.autorennenfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+    opens com.example.autorennenfx to javafx.fxml;
+    exports com.example.autorennenfx;
+}
